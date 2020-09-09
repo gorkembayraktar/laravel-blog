7cd1c8135f0d0741d9473cc6ac77e69275e6777f
Grkm <bygrkm63@gmail.com>
2020-09-09 22:36:21 +0300

    deploy

---
+++ app/Http/Controllers/App.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class App extends Controller
+{
+    //
+    public function index(){
+        echo 'anasayfa';
+    }
+    public function hakkimizda(){
+        echo 'hakkimizda';
+    }
+}
---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $categories = ['Eğlence','Bilişim','Gezi','Teknoloji','Sağlık','Sport','Günlük Yaşam'];
+        foreach($categories as $category):
+            DB::table('categories')->insert([
+                'name'=>$category,
+                'slug'=>Str::slug($category)
+            ]);
+            endforeach;
+       
+    }
+}
